dd91d060475cd623ea701bfd5706b3a14d907610
jerryskye <jarek@siedlarz.com>
2017-10-19 01:42:55 +0200

    added deadlines

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170924165051) do
+ActiveRecord::Schema.define(version: 20171018233014) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.datetime "deadline"
   end
 
 end
